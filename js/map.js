cf53c4dd062aa4bd8c55ed9b9ce5183541f69183
Антон <antonvolynskii@gmail.com>
2020-02-09 23:49:41 +1000

    Изменил название проверки

--- js/map.js
+++ js/map.js
@@ -128,7 +128,7 @@ inputCapacity.addEventListener('change', function(evt) {
         inputCapacity.children[2].disabled = true;
     }
 });
-    console.log('123')
+    console.log('changes')
 //here i set a custom validity for input  
  })();
     
